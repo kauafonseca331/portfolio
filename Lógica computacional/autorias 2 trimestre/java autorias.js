419dd11c18abf5a32255a9c60ebd52f5b96fefe5
kauafonseca331 <131172130+kauafonseca331@users.noreply.github.com>
2023-07-04 12:04:34 -0300

    Add files via upload

---
+++ Lógica computacional/autorias 2 trimestre/java autorias.js
@@ -0,0 +1,50 @@
+/* Bloco do var */
+var nome = "kauã" // Declarando a variável
+console.log(nome)
+
+var nome = "fonseca" // Recriando virtual 
+console.log(nome)
+
+nome = "kauã fonseca" // retribuindo valor 
+console.log(nome)
+
+/* Bloco do let
+function getNome(){
+
+    let nome = "kauã";
+    
+    if (nome === 'kauã') {
+        let nome = 'kauã fonseca';
+
+        console.log('Dentro do if: ' + nome);
+}
+console.log('Fora do if: ' + nome);
+
+}
+getNome()
+*/
+let nome
+console.log(nome)
+
+/*Bloco de const */
+const nome = 'kauã'
+console.log(nome)
+
+const nome = 'fonseca'
+console.log(nome)
+
+const nome = 'kauã'
+console.log(nome)
+
+nome = 'fonseca'
+console.log(nome)
+
+/*
+const nome = 'kaua'
+console.log(nome)
+
+nome = 'fonseca'
+console.log(nome)
+*/
+
+
